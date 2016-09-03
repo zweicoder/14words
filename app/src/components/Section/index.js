ec7818fe3deb505fb5d4c205c9e4ba6fdb525f5c
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2016-09-03 11:45:01 +0800

    Clean up Section

--- app/src/components/Section/index.js
+++ app/src/components/Section/index.js
@@ -3,11 +3,11 @@ import styles from './styles.css'
 
 // Dumb component that renders a section with header, optional message, result and an input
 function Section(props) {
-  const { header, message, result, inputPlaceholder, onInputChange } = props;
+  const { header, message, result, inputPlaceholder, onInputChange, ..._props } = props;
   return <section className={styles.section} {..._props}>
     <div className={styles.header}> {header}</div>
     <input placeholder={inputPlaceholder} size="30" onChange={onInputChange}/>
-    {{ message } ? <span className={styles.message}>{message}</span> : null}
-    {{ result } ? <span className={styles.result}>{result}</span> : null}
+    { message  ? <span className={styles.message}>{message}</span> : null}
+    { result  ? <span className={styles.result}>{result}</span> : null}
   </section>
 }
