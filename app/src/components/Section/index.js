684cdd8a11cac9d5eb50ccb03a638d61d517ee2a
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2016-09-03 11:41:45 +0800

    Modularize Section

---
+++ app/src/components/Section/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './styles.css'
+
+// Dumb component that renders a section with header, optional message, result and an input
+function Section(props) {
+  const { header, message, result, inputPlaceholder, onInputChange } = props;
+  return <section className={styles.section} {..._props}>
+    <div className={styles.header}> {header}</div>
+    <input placeholder={inputPlaceholder} size="30" onChange={onInputChange}/>
+    {{ message } ? <span className={styles.message}>{message}</span> : null}
+    {{ result } ? <span className={styles.result}>{result}</span> : null}
+  </section>
+}
---
+++ app/src/containers/EncoderSection/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Section from 'components/Section';
+import { fetchEncoded } from 'utils/encoderService';
+
+class EncoderSection extends React.Component {
+  constructor(props) {
+    super(props);
+    const { mode, ..._props } = this.props;
+    this.state = {};
+  }
+
+  encodeAddress = (e) => {
+    this.setState({ message: null });
+    this.setState({ result: null });
+
+    const inp = e.target.value;
+    if (!inp) {
+      return;
+    }
+
+    if (!inp.match(/^0x[a-fA-F0-9]{40}$/)) {
+      this.setState({ message: `Not a valid Ethereum Address!` });
+      return;
+    }
+
+    fetchEncoded(inp, mode).then((res)=> {
+      this.setState({ result: res })
+    })
+      .catch((err)=> {
+        this.setState({ message: err })
+      })
+  };
+
+  render() {
+    return <Section
+      header="Turn your Ethereum Address into 14 words!"
+      inputPlaceholder="Put Your Address Here"
+      onChange={this.encodeAddress}
+      message={this.state.message}
+      result={this.state.result}
+    />
+  }
+}
+
+EncoderSection.propTypes = {
+  mode: React.PropTypes.func.isRequired
+};
